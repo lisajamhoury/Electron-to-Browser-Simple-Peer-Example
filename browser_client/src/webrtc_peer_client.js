414ed10b9da1afabe300c03713ce034cf5830f44
Lisa Jamhoury <lisajamhoury@gmail.com>
2019-12-17 11:41:53 -0500

    Working on localhost over https

--- browser_client/src/webrtc_peer_client.js
+++ browser_client/src/webrtc_peer_client.js
@@ -1,12 +1,20 @@
-const io = require('socket.io-client');
-let Peer = require('simple-peer');
+const io = require("socket.io-client");
+let Peer = require("simple-peer");
 
-const socket = io.connect('http://localhost:3000');
-let peer; 
-const room = 'foo'; // Could prompt for room name: // room = prompt('Enter room name:');
+const options = {
+  secure: true,
+  reconnect: true,
+  rejectUnauthorized: false
+};
 
-let localVideo = document.querySelector('#localVideo');
-let remoteVideo = document.querySelector('#remoteVideo');
+// use localhost, ip on local network, or ngrok address
+// ie. https://8bf250a1.ngrok.io/, more here https://dashboard.ngrok.com/get-started
+const socket = io.connect("https://localhost:443", options);
+let peer;
+const room = "foo"; // Could prompt for room name: // room = prompt('Enter room name:');
+
+let localVideo = document.querySelector("#localVideo");
+let remoteVideo = document.querySelector("#remoteVideo");
 let localStream;
 
 let isChannelReady = false;
@@ -15,105 +23,107 @@ let isStarted = false;
 let turnReady; // currently unused
 
 const pcConfig = {
-  'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
-  }]
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302"
+    }
+  ]
 };
 
-/////////////////// Client Signal Server Using Socket IO /////////////////// 
-
+/////////////////// Client Signal Server Using Socket IO ///////////////////
 
 // starts socket client communication with signal server automatically
-if (room !== '') {
-  socket.emit('create or join', room);
-  console.log('Attempted to create or join room', room);
+if (room !== "") {
+  socket.emit("create or join", room);
+  console.log("Attempted to create or join room", room);
 }
 
-socket.on('created', room => {
-  console.log('Created room ' + room);
+socket.on("created", room => {
+  console.log("Created room " + room);
   isInitiator = true;
 });
 
 // room only holds two clients, can be changed in signal_socket.js
-socket.on('full', room => {
-  console.log('Room ' + room + ' is full');
+socket.on("full", room => {
+  console.log("Room " + room + " is full");
 });
 
-// called by initiator client only 
-socket.on('join', room => {
-  console.log('Another peer made a request to join room ' + room);
-  console.log('This peer is the initiator of room ' + room + '!');
+// called by initiator client only
+socket.on("join", room => {
+  console.log("Another peer made a request to join room " + room);
+  console.log("This peer is the initiator of room " + room + "!");
   isChannelReady = true;
 });
 
-// called by non-initiator client 
-socket.on('joined', room => {
-  console.log('joined: ' + room);
+// called by non-initiator client
+socket.on("joined", room => {
+  console.log("joined: " + room);
   isChannelReady = true;
 });
 
-// logs messages from server 
-socket.on('log', array => {
+// logs messages from server
+socket.on("log", array => {
   console.log.apply(console, array);
 });
 
 // This client receives a message
-socket.on('message', message => {
-  console.log('MESSAGE', message);
+socket.on("message", message => {
+  console.log("MESSAGE", message);
 
   if (message.type) {
-    console.log('received msg typ ', message.type)     } else {
-      console.log('Client received message:', message);
-  } 
+    console.log("received msg typ ", message.type);
+  } else {
+    console.log("Client received message:", message);
+  }
 
-  if (message === 'got user media') {
+  if (message === "got user media") {
     maybeStart();
-  } else if (message.type === 'sending signal') {
-      console.log('receiving simple signal data');
-      
-      if (!peer) {
-        createPeerConnection(isInitiator);
-        peer.signal(message.data);
-      } else {
-        peer.signal(message.data);  
-      }
-  } else if (message === 'bye' && isStarted) {
+  } else if (message.type === "sending signal") {
+    console.log("receiving simple signal data");
+
+    if (!peer) {
+      createPeerConnection(isInitiator);
+      peer.signal(message.data);
+    } else {
+      peer.signal(message.data);
+    }
+  } else if (message === "bye" && isStarted) {
     handleRemoteHangup();
   }
 });
 
-
 function sendMessage(message) {
-  console.log('Client sending message: ', message);
-  socket.emit('message', message);
+  console.log("Client sending message: ", message);
+  socket.emit("message", message);
 }
 
-/////////////////// getUserMedia starts video and starts Simple Peer on Connection  /////////////////// 
+/////////////////// getUserMedia starts video and starts Simple Peer on Connection  ///////////////////
 
-navigator.mediaDevices.getUserMedia({
-  audio: false,
-  video: true
-})
-.then(gotStream)
-.catch(function(e) {
-  alert('getUserMedia() error: ' + e.name);
-});
+navigator.mediaDevices
+  .getUserMedia({
+    audio: false,
+    video: true
+  })
+  .then(gotStream)
+  .catch(function(e) {
+    alert("getUserMedia() error: " + e.name);
+  });
 
 function gotStream(stream) {
-  console.log('Adding local stream.');
+  console.log("Adding local stream.");
   localStream = stream;
   localVideo.srcObject = stream;
-  sendMessage('got user media');
+  sendMessage("got user media");
   if (isInitiator) {
     maybeStart();
   }
 }
 
 function maybeStart() {
-  console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
-  if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
-    console.log('>>>>>> creating peer connection');
-    console.log('isInitiator', isInitiator);
+  console.log(">>>>>>> maybeStart() ", isStarted, localStream, isChannelReady);
+  if (!isStarted && typeof localStream !== "undefined" && isChannelReady) {
+    console.log(">>>>>> creating peer connection");
+    console.log("isInitiator", isInitiator);
 
     createPeerConnection(isInitiator);
     isStarted = true;
@@ -121,42 +131,39 @@ function maybeStart() {
 }
 
 window.onbeforeunload = function() {
-  sendMessage('bye');
+  sendMessage("bye");
 };
 
-
 function createPeerConnection(isInit) {
-
-  peer = new Peer({initiator: isInit, stream: localStream});
-  console.log('creating simple peer');
+  peer = new Peer({ initiator: isInit, stream: localStream });
+  console.log("creating simple peer");
 
   // If isInitiator,peer.on'signal' will fire right away, if not it waits for signal
   // https://github.com/feross/simple-peer#peeronsignal-data--
-  peer.on('signal', data => sendSignal(data));
-  peer.on('connect', data => console.log('SIMPLE PEER IS CONNECTED', data));
-  peer.on('error', err => console.log(err));
-  peer.on('stream', stream => remoteVideo.srcObject = stream);
-  peer.on('close', () => hangup());
-
+  peer.on("signal", data => sendSignal(data));
+  peer.on("connect", data => console.log("SIMPLE PEER IS CONNECTED", data));
+  peer.on("error", err => console.log(err));
+  peer.on("stream", stream => (remoteVideo.srcObject = stream));
+  peer.on("close", () => hangup());
 }
 
 function sendSignal(data) {
-  console.log('sending signal');
+  console.log("sending signal");
 
   sendMessage({
-    type: 'sending signal',
+    type: "sending signal",
     data: JSON.stringify(data)
   });
 }
 
 function hangup() {
-  console.log('Hanging up.');
+  console.log("Hanging up.");
   stop();
-  sendMessage('bye');
+  sendMessage("bye");
 }
 
 function handleRemoteHangup() {
-  console.log('Session terminated.');
+  console.log("Session terminated.");
   stop();
   isInitiator = false;
 }
@@ -167,41 +174,43 @@ function stop() {
   peer = null;
 }
 
+/////////////////// Turn Server Used if Not on LocaHost — I have not tested this  ///////////////////
 
-/////////////////// Turn Server Used if Not on LocaHost — I have not tested this  /////////////////// 
-
-
-if (location.hostname && location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
+if (
+  location.hostname &&
+  location.hostname !== "localhost" &&
+  location.hostname !== "127.0.0.1"
+) {
   requestTurn(
-    'https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913'
+    "https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913"
   );
 }
 
 function requestTurn(turnURL) {
   let turnExists = false;
   for (let i in pcConfig.iceServers) {
-    if (pcConfig.iceServers[i].urls.substr(0, 5) === 'turn:') {
+    if (pcConfig.iceServers[i].urls.substr(0, 5) === "turn:") {
       turnExists = true;
       turnReady = true;
       break;
     }
   }
   if (!turnExists) {
-    console.log('Getting TURN server from ', turnURL);
+    console.log("Getting TURN server from ", turnURL);
     // No TURN server. Get one from computeengineondemand.appspot.com:
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
         let turnServer = JSON.parse(xhr.responseText);
-        console.log('Got TURN server: ', turnServer);
+        console.log("Got TURN server: ", turnServer);
         pcConfig.iceServers.push({
-          'urls': 'turn:' + turnServer.username + '@' + turnServer.turn,
-          'credential': turnServer.password
+          urls: "turn:" + turnServer.username + "@" + turnServer.turn,
+          credential: turnServer.password
         });
         turnReady = true;
       }
     };
-    xhr.open('GET', turnURL, true);
+    xhr.open("GET", turnURL, true);
     xhr.send();
   }
 }
--- electron_app/signal_socket.js
+++ electron_app/signal_socket.js
@@ -1,68 +1,75 @@
 //////// socket signal server ///////////
 
-'use strict';
+"use strict";
 
-var os = require('os');
+const os = require("os");
+const fs = require("fs");
+const https = require("https");
 
-const server = require('http').createServer();
-const ioServer = require('socket.io')(server);
-server.listen(3000);
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem")
+};
 
-console.log('server created');
+const server = https.createServer(options);
+const ioServer = require("socket.io")(server);
+server.listen(443);
 
-ioServer.sockets.on('connection', function(socket) {
+console.log("server created");
 
+ioServer.sockets.on("connection", function(socket) {
   // convenience function to log server messages on the client
   function log() {
-    var array = ['Message from server:'];
+    var array = ["Message from server:"];
     array.push.apply(array, arguments);
-    socket.emit('log', array);
+    socket.emit("log", array);
   }
 
-  socket.on('message', function(message) {
-    console.log('Client said: ', message);
+  socket.on("message", function(message) {
+    console.log("Client said: ", message);
     // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
+    socket.broadcast.emit("message", message);
   });
 
-  socket.on('create or join', function(room) {
-    console.log('Received request to create or join room ' + room);
+  socket.on("create or join", function(room) {
+    console.log("Received request to create or join room " + room);
 
-    // I can't console.log or debug this. Why? 
+    // I can't console.log or debug this. Why?
     var clientsInRoom = ioServer.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    var numClients = clientsInRoom
+      ? Object.keys(clientsInRoom.sockets).length
+      : 0;
+    console.log("Room " + room + " now has " + numClients + " client(s)");
 
     if (numClients === 0) {
       socket.join(room);
-      console.log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-
-    } else if (numClients === 1) { // change number of clients allowed in room here 
-      console.log('Client ID ' + socket.id + ' joined room ' + room);
-      ioServer.sockets.in(room).emit('join', room);
+      console.log("Client ID " + socket.id + " created room " + room);
+      socket.emit("created", room, socket.id);
+    } else if (numClients === 1) {
+      // change number of clients allowed in room here
+      console.log("Client ID " + socket.id + " joined room " + room);
+      ioServer.sockets.in(room).emit("join", room);
       socket.join(room);
-      socket.emit('joined', room, socket.id);
-      ioServer.sockets.in(room).emit('ready'); // not being used anywhere
-    } else { // max two clients
-      socket.emit('full', room);
+      socket.emit("joined", room, socket.id);
+      ioServer.sockets.in(room).emit("ready"); // not being used anywhere
+    } else {
+      // max two clients
+      socket.emit("full", room);
     }
-
   });
 
-  socket.on('ipaddr', function() {
+  socket.on("ipaddr", function() {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
       ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
+        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
+          socket.emit("ipaddr", details.address);
         }
       });
     }
   });
 
-  socket.on('bye', function(){
-    console.log('received bye');
+  socket.on("bye", function() {
+    console.log("received bye");
   });
-
 });
